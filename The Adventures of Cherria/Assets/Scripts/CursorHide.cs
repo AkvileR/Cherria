bf2b13b53b8a3a5c872d4447049333c9ef8738e3
AkvileR <56289629+AkvileR@users.noreply.github.com>
2020-03-19 14:30:50 +0200

    Add existing file

---
+++ The Adventures of Cherria/Assets/Scripts/CursorHide.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CursorHide : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Cursor.visible = false;
+      }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ The Adventures of Cherria/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int maxHealth = 20;
+    public int currentHealth;
+    public HealthBar healthBar;
+    public CharacterController2D controller;
+    public Animator animator;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    void Update () {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed",Mathf.Abs( horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+       
+    }
+   
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime,false,jump);
+        jump = false;
+    }
+
+
+
+     void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+
+    }
+    
+
+    public void TakeDamage (int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag =="Instakill")
+        {
+            RestartLevel();
+        }
+    }
+
+    void RestartLevel ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
+
