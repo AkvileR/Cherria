f3fd9ef3df54d006bc0f89362182b19bd5ef3a7f
AkvileR <56289629+AkvileR@users.noreply.github.com>
2020-04-28 14:08:01 +0300

    Frederikas
    Pilnai veikiantis Frederikas (Padaryta,kad galėtų kalbėti)

---
+++ The Adventures of Cherria/Assets/Scripts/Frederick.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Frederick : MonoBehaviour
+{ 
+
+    public GameObject popUpBox;
+    public TMP_Text dialogText;
+    public string dialog;
+    public bool playerInRange;
+    public string popUp;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        popUpBox.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I) && playerInRange)
+        {
+            if (popUpBox.activeInHierarchy)
+            {
+                popUpBox.SetActive(false);
+            }
+            else
+            {
+                popUpBox.SetActive(true);
+                dialogText.text = dialog;
+            }
+            
+            
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            popUpBox.SetActive(false);
+        }
+    }
+
+    public void Close()
+    {
+        popUpBox.SetActive(false);
+    }
+    
+}
